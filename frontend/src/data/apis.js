609e44b0c53c3bd692219e574a3ed1d27fc4798d
Rabyn-Collab <babynshrestha64@gmail.com>
2024-12-05 19:50:46 +0545

    changes

--- frontend/src/data/apis.js
+++
@@ -1,7 +0,0 @@
-
-//url
-export const base = 'http://192.168.1.64:5000';
-export const baseUrl = `${base}/api`;
-
-
-
--- frontend/src/features/About.jsx
+++
@@ -1,9 +0,0 @@
-const About = () => {
-  return (
-    <div>
-      <h1>slkdalk;asd</h1>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure, quaerat quia voluptatum, eius ab assumenda quos harum pariatur, hic animi ducimus ipsa deleniti. Placeat ad iste dolor eos sed molestias?</p>
-    </div>
-  )
-}
-export default About
