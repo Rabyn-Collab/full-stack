76abeac41bff82acbfd08683d6758b329462ee25
Rabyn-Collab <babynshrestha64@gmail.com>
2024-12-05 19:57:14 +0545

    changes

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://mern-shrawan.vercel.app'],
+  origin: ['http://localhost:3000', 'https://full-stack-five-delta.vercel.app'],
   credentials: true,
 }));
 app.use(cookieParser());
